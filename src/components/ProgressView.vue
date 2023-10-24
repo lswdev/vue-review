5ef9020c13603d60c105533d9de8787040be7858
lswsepia <lswsepia@gmail.com>
2023-10-24 11:30:02 +0900

    ProgressView 컴포넌트 추가

--- src/components/ProgressView.vue
+++ src/components/ProgressView.vue
@@ -3,43 +3,55 @@
   <section>
     <ul class="progressContain">
       <li>
-        <div></div>
-        <div></div>
+        <img :src="iconFirst" alt="icon-engineer">
+        <div>수리기사</div>
       </li>
-      <li>2</li>
+      <li></li>
       <li>
-        <div></div>
-        <div></div>
+        <img :src="iconSecond" alt="icon-clock">
+        <div>시간약속</div>
       </li>
-      <li>4</li>
+      <li></li>
       <li>
-        <div></div>
-        <div></div>
+        <img :src="iconThird" alt="icon-recycle">
+        <div>대기환경</div>
       </li>
-      <li>6</li>
+      <li></li>
       <li>
-        <div></div>
-        <div></div>
+        <img :src="iconFourth" alt="icon-person">
+        <div>응대수준</div>
       </li>
-      <li>8</li>
+      <li></li>
       <li>
-        <div></div>
-        <div></div>
+        <img :src="iconFifth" alt="icon-pages">
+        <div>추가사항</div>
       </li>
     </ul>
+
   </section>
 </template>
 
 <script>
+  export default {
+    data: () => ({
+      iconFirst: require('../assets/icons/icon-engineer.svg'),
+      iconSecond: require('../assets/icons/icon-clock-grey.svg'),
+      iconThird: require('../assets/icons/icon-recycle-grey.svg'),
+      iconFourth: require('../assets/icons/icon-person-grey.svg'),
+      iconFifth: require('../assets/icons/icon-pages-grey.svg'),
+    }),
+    methods: {
+
+    }
+  }
 
 </script>
 
 <style scoped>
   section {
-    margin: 40px auto;
+    margin: 40px auto 80px;
+    padding: 40px 0 10px;
     width: 1150px;
-    border: 1px solid red;
-    box-sizing: border-box;
   }
   .progressContain {
     display: flex;
@@ -50,11 +62,17 @@
   .progressContain > li:nth-of-type(odd) {
     width: 60px;
     height: 60px;
-    background-color: yellow;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 0.5rem;
   }
   .progressContain > li:nth-of-type(even) {
     width: 96px;
     height: 10px;
-    background-color: green;
+    background: #F5F5F5;
+  }
+  .progressContain > li > div {
+    white-space: nowrap;
   }
 </style>
