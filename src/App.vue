6c35400c388a796dab44db91c5a13af43e8e2287
lswsepia <lswsepia@gmail.com>
2023-10-24 11:29:45 +0900

    세팅값 변경 및 ContentView 컴포넌트 추가

--- src/App.vue
+++ src/App.vue
@@ -2,29 +2,42 @@
   <div id="app">
     <HeaderView></HeaderView>
     <ProgressView></ProgressView>
+    <ContentView></ContentView>
   </div>
 </template>
 
 <script>
 import HeaderView from './components/HeaderView.vue'
 import ProgressView from './components/ProgressView.vue'
+import ContentView from './components/ContentView.vue'
 
 export default {
   name: 'App',
-  components: { HeaderView, ProgressView }
+  components: { HeaderView, ProgressView, ContentView }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-}
-
-body, html { margin: 0; padding: 0; }
-* { text-decoration: none; }
-ul { margin: 0; padding: 0; }
-li { list-style: none; }
-label, main { display: block; }
-table { border-collapse: collapse;}
+  @font-face {
+    font-family: 'Pretendard-Regular';
+    src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff') format('woff');
+    font-weight: 400;
+    font-style: normal;
+  }
+  body, html {
+    font-family: 'Pretendard-Regular';
+    margin: 0; padding: 0;
+    color: #222222;
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: normal;
+    letter-spacing: -0.0225rem;
+  }
+  * { text-decoration: none; }
+  ul { margin: 0; padding: 0; }
+  li { list-style: none; }
+  label, main { display: block; }
+  table { border-collapse: collapse;}
 
 </style>
---
+++ src/components/ContentView.vue
@@ -0,0 +1,65 @@
+
+<template>
+  <section>
+    <div>A/S 만족도 설문</div>
+    <div>
+      홍길동 고객님 2023년 10월 25일 차량 A/S 수리는 잘 받으셨나요? 차량 수리에 대한 만족도 설문을 요청 드립니다.<br>
+      설문 결과는 더 나은 서비스를 위한 기초 자료로 사용할 예정입니다. 감사합니다.
+    </div>
+    <div class="questionContain">
+      <p>{{ questionTitle }}</p>
+      <ul class="questions" v-for="questions in questionList" :key="questions.id">
+        <li>
+          <input type="radio" value=""> <span v-text="questions.text"></span>
+        </li>
+      </ul>
+    </div>
+  </section>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      questionList: [
+        { text: '매우 상세하게 설명' },
+        { text: '비교적 상세하게 설명' },
+        { text: '일반적으로 설명' },
+        { text: '대충 설명함' },
+        { text: '설명 안함' },
+      ],
+      questionTitle: '1. 수리기사는 수리 내용에 대해 상세하게 설명해 주었나요?',
+    }),
+    methods: {
+
+    }
+  }
+</script>
+
+<style scoped>
+  section {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    margin: 40px auto 80px;
+    width: 1150px;
+    font-size: 1.25rem;
+  }
+  section > div {
+    margin-bottom: 40px;
+  }
+  section > div:first-child {
+    font-size: 1.55rem;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+    letter-spacing: -0.04rem;
+  }
+  .questionContain {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+  }
+  .questionContain > p {
+    margin-top: 0;
+  }
+</style>
